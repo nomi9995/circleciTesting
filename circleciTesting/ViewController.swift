813b752f796698b00850542129d37dfd3295efaa
Muhammad Numan <muhammadnuman70@gmail.com>
2018-10-26 09:33:35 +0500

    Initial Commit

---
+++ circleciTesting/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  circleciTesting
+//
+//  Created by Muhammad Numan on 10/26/18.
+//  Copyright © 2018 Muhammad Numan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
